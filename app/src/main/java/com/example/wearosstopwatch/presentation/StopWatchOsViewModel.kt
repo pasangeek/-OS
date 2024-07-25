e088a322670bb3b2f4630163a80900fc94e215f2
pasangeek <pasan.madusanka9@gmail.com>
2024-07-25 22:33:41 +0900

    implement time difference catch

--- app/src/main/java/com/example/wearosstopwatch/presentation/StopWatchOsViewModel.kt
+++ app/src/main/java/com/example/wearosstopwatch/presentation/StopWatchOsViewModel.kt
@@ -1,11 +1,33 @@
 package com.example.wearosstopwatch.presentation
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.flow
 
 class StopWatchOsViewModel : ViewModel() {
     private val _elapsedTime = MutableStateFlow(0L)
     private val _timerState = MutableStateFlow(TimerSate.RESET)
     val timerSate = _timerState.asStateFlow()
+
+
+
+    //emit time differences
+    private fun getTimerFlow(isRunning: Boolean): Flow<Long> {
+        return flow {
+            var startMillis = System.currentTimeMillis()
+            while (isRunning) {
+                val currentMillis = System.currentTimeMillis()
+                val timeDiff = if (currentMillis > startMillis) {
+                    currentMillis - startMillis
+                } else 0L
+                emit(timeDiff)
+                startMillis = System.currentTimeMillis()
+                delay(10L)
+            }
+        }
+    }
+
 }

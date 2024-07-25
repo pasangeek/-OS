b1a82ec7ed3f19712ec65223d2ae2857a8be8fb3
pasangeek <pasan.madusanka9@gmail.com>
2024-07-25 23:12:31 +0900

    implement date time formatter

--- app/src/main/java/com/example/wearosstopwatch/presentation/StopWatchOsViewModel.kt
+++ app/src/main/java/com/example/wearosstopwatch/presentation/StopWatchOsViewModel.kt
@@ -6,12 +6,21 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
 
 class StopWatchOsViewModel : ViewModel() {
     private val _elapsedTime = MutableStateFlow(0L)
     private val _timerState = MutableStateFlow(TimerSate.RESET)
-    val timerSate = _timerState.asStateFlow()
 
+    val timerSate = _timerState.asStateFlow()
+    private val formatter = DateTimeFormatter.ofPattern("HH:mm:ss:SSS")
+    val stopWatchText = _elapsedTime
+        .map { millis ->
+            LocalTime.ofNanoOfDay(millis * 1_000_000).format(formatter)
+
+        }
 
 
     //emit time differences

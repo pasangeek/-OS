1ae5205b3d792c99743425901746d6fcf4b38ea4
pasangeek <pasan.madusanka9@gmail.com>
2024-07-27 23:21:07 +0900

    implement compose functions for preview

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,7 @@ dependencies {
     androidTestImplementation(libs.ui.test.junit4)
     debugImplementation(libs.ui.tooling)
     debugImplementation(libs.ui.test.manifest)
+    implementation(libs.lifecycle.viewmodel.compose)
+    implementation(libs.lifecycle.runtime.compose)
+    implementation (libs.material.icons.extended)
 }
--- app/src/main/java/com/example/wearosstopwatch/presentation/MainActivity.kt
+++ app/src/main/java/com/example/wearosstopwatch/presentation/MainActivity.kt
@@ -9,23 +9,40 @@ package com.example.wearosstopwatch.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Pause
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.Stop
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.wear.compose.material.Button
+import androidx.wear.compose.material.ButtonDefaults
+import androidx.wear.compose.material.Icon
 import androidx.wear.compose.material.MaterialTheme
+import androidx.wear.compose.material.Scaffold
 import androidx.wear.compose.material.Text
 import androidx.wear.compose.material.TimeText
-import com.example.wearosstopwatch.R
-import com.example.wearosstopwatch.presentation.theme.WearOsStopWatchTheme
+import androidx.wear.compose.material.TimeTextDefaults
+import androidx.wear.compose.material.Vignette
+import androidx.wear.compose.material.VignettePosition
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,41 +51,98 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setTheme(android.R.style.Theme_DeviceDefault)
-/*
-        setContent {
-            WearApp("Android")
-        }*/
-    }
-}
-/*
 
-@Composable
-fun WearApp(greetingName: String) {
-    WearOsStopWatchTheme {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(MaterialTheme.colors.background),
-            contentAlignment = Alignment.Center
-        ) {
-            TimeText()
-            Greeting(greetingName = greetingName)
+        setContent {
+            val viewModel = viewModel<StopWatchOsViewModel>()
+            val timerSate by viewModel.timerSate.collectAsStateWithLifecycle()
+            val stopWatchText by viewModel.stopWatchText.collectAsStateWithLifecycle()
+            Scaffold(
+                timeText = {
+                    TimeText(
+                        timeTextStyle = TimeTextDefaults.timeTextStyle(
+                            fontSize = 10.sp
+                        )
+                    )
+                },
+                vignette = {
+                    Vignette(vignettePosition = VignettePosition.TopAndBottom)
+                }
+            ) {
+                StopWatch(
+                    state = timerSate,
+                    text = stopWatchText,
+                    onToggleRunning = viewModel::toggleIsRunning,
+                    onReset = viewModel::resetTimer,
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
         }
     }
 }
 
 @Composable
-fun Greeting(greetingName: String) {
-    Text(
-        modifier = Modifier.fillMaxWidth(),
-        textAlign = TextAlign.Center,
-        color = MaterialTheme.colors.primary,
-        text = stringResource(R.string.hello_world, greetingName)
+private fun StopWatch(
+
+    state: TimerSate,
+    text: String,
+    onToggleRunning: () -> Unit,
+    onReset: () -> Unit,
+    modifier: Modifier = Modifier
+
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = text,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.SemiBold,
+            textAlign = TextAlign.Center
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Button(onClick = onToggleRunning) {
+                Icon(
+                    imageVector = if (state == TimerSate.RUNNING) {
+                        Icons.Default.Pause
+                    } else {
+                        Icons.Default.PlayArrow
+                    },
+                    contentDescription = null
+                )
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            Button(
+                onClick = onReset,
+                enabled = state != TimerSate.RESET,
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = MaterialTheme.colors.surface
+                )
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Stop,
+                    contentDescription = null
+                )
+            }
+        }
+    }
+}
+/**
+ * Preview for the StopWatch composable function.
+ */
+@Preview(showBackground = true)
+@Composable
+fun StopWatchPreview() {
+    StopWatch(
+        state = TimerSate.RESET,
+        text = "00:00:00:000",
+        onToggleRunning = {},
+        onReset = {},
+        modifier = Modifier.fillMaxSize()
     )
 }
-
-@Preview(device = Devices.WEAR_OS_SMALL_ROUND, showSystemUi = true)
-@Composable
-fun DefaultPreview() {
-    WearApp("Preview Android")
-}*/
